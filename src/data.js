b9476c4af707c1942de00a33840866b573455cea
carttman14 <carttman14@gmail.com>
2022-02-09 05:35:01 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,111 @@
+const reviews=[
+    {
+        id:1,
+        name:'Juan Diaz',
+        job:'Full stack developer',
+        image:'https://i.ytimg.com/vi/TvLxIvNGGJA/maxresdefault.jpg',
+        text:'Comencé esta aventura para dar soporte a mis alumnos presenciales. Con el apoyo de todos los que me seguís mi canal de YouTube y esta web han crecido mucho más de lo que yo esperaba. Gracias a todos por estar ahí.'
+    },
+       {
+        id:2,
+        name:'Juan Villalvazo',
+        job:'Software Engineer',
+        image:'https://2019.talent-land.mx/wp-content/uploads/2019/02/tl2019-speaker-developer-Juan-Villalvazo.jpg',
+        text:'Ingeniero en Computación e Instructor profesional de cursos de programación, Youtuber y Conferencista Internacional enfocado a la motivación y apoyo tanto de programadores como de emprendedores.'
+    },
+
+      {
+        id:3,
+        name:'Ignacio Gutierrez',
+        job:'Informatic content creator',
+        image:'https://pbs.twimg.com/media/FCXhgsdWUAQ4KBx.jpg',
+        text:'Bluuweb es una agencia de desarrollo y diseño web con mas de 5 años de experiencia. Que ofrece soluciones innovadoras y de alta calidad a sus clientes. Compuesto por un equipo de profesionales de primer nivel, especializados en el desarrollo de sitios, aplicaciones dinámicas y asesorías en Marketing Digital.'
+    },
+        {
+        id:4,
+        name:'John Smilga',
+        job:'React developer',
+        image:'https://pbs.twimg.com/profile_images/1022238527960973313/y2H7ucaa_400x400.jpg',
+        text:'There are so many resources in the world of tech that it hard to sort through and pick the right resource for you. There are so many courses ve put together a list of some of my favorite teachers in the JavaScript, and frontend development, community. Feel free to leave your recommendations below!'
+    },
+       {
+        id:5,
+        name:'Lucas Dalto',
+        job:'Web developer',
+        image:'https://yt3.ggpht.com/ytc/AKedOLQsBVcXS5FKCyRczc5lbHeviajNWX-gzbUleR1NqA=s900-c-k-c0x00ffffff-no-rj',
+        text:'Soy Lucas Dalto. EduTuber de ciencia y tecnología, Programador, Growth Hacker, y con el canal de programacion y desarrollo mas grande de Argentina'
+    },
+       {
+        id:6,
+        name:'Lucas Moy',
+        job:'Backend Dev',
+        image:'https://yt3.ggpht.com/5bcgRHFWGFIh_NqZueyqUoZvLIiYrdjBXmm2LLrETnRv94VHRqYpAUutavtmafNAiDIXgPZrVzo=s900-c-k-c0x00ffffff-no-rj',
+        text:'!Hola¡ En esta página web publicaré todos mis cursos de programación. Si tienes alguna pregunta no dudes en consultarme.'
+    },
+     {
+        id:7,
+        name:'John Mircha',
+        job:'Full stack dev',
+        image:'https://yt3.ggpht.com/ytc/AKedOLSv8c8GSrfg2HsQLULN6rvT-MJdI55B23blpzYpaA=s900-c-k-c0x00ffffff-no-rj',
+        text:'Desde 2004 tengo experiencia en el área del desarrollo web y la educación. Imparto clases, conferencias y capacitaciones para personas, empresas, instituciones educativas y comunidades en Internet'
+    },
+       {
+        id:8,
+        name:'Miguel Durán',
+        job:'Frontend Developer',
+        image:'https://static-cdn.jtvnw.net/jtv_user_pictures/5a72c3d9-424a-40a6-bf23-73370cd85578-profile_image-300x300.png',
+        text:'Esta frase fue la contestación de Linus en una acalorada discusión con Jamie Lokier. Estaban hablando sobre cómo mejorar la creación de hilos en Linux. Tras intercambiar algunos mensajes y después de una extensa contestación de Jamie Lokier, entonces Linus dijo esa famosa frase: Hablar es barato. Muéstrame el código'
+    },
+     {
+        id:9,
+        name:'Alejandro Taborda',
+        job:'Backend developer',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw0E4lC06yZypKcVpongcNU_U5obaHLEeQ-M0ocADfarathgbhpbdE7kpF50fZVis0-RQ&usqp=CAU',
+        text:'Alejandro Taboada Sánchez, más conocido como Programación ATS, fue un estudiante de estudiante de Ingeniería informática en la Universidad Nacional de Trujillo (PERU), programador y Youtuber. Su canal se denomina “Programación ATS” y en vida Alejandro publicó tutoriales sobre programación de computadoras en diversos lenguajes de programación, muchos de esos tutoriales han ayudado a miles de estudiantes a comprender mejor la codificación y lógica para programar.'
+    },
+     {
+        id:10,
+        name:'Victor Aranna',
+        job:'Fullstack developer',
+        image:'https://scontent.fclo1-4.fna.fbcdn.net/v/t1.6435-9/103546233_4167439576602749_9173722053592836237_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHMFPsBrwXLnhI0b7bLHjY2QULBELzRnlNBQsEQvNGeU_B0j5oQidC2eMrHgf51XZ00UMAfUR_bpzRQap09UBxT&_nc_ohc=DhMoc9WfcdUAX-eq1u6&_nc_ht=scontent.fclo1-4.fna&oh=00_AT87uI93w4bjY9T32qmEK7kBT_ZNGxU6T0GyOLDCUgfJCQ&oe=6226C107',
+        text:'Codersfree pretende ofrecer la mejor experiencia de compra y estudio a cada uno de sus usuarios. Por ello nos esforzamos encarecidamente en proteger su privacidad presentando este documento donde detallamos nuestros procedimientos y políticas respecto a la información y datos personales. Datos que son recopilados a través del sitio web y/o plataformas virtuales de Codersfree.'
+    },
+    {
+        id:11,
+        name:'Oscar Uh',
+        job:'Php Developer',
+        image:'https://pbs.twimg.com/profile_images/1449620017721511936/af7sQruu_400x400.jpg',
+        text:'Ingeniero en sistemas computacionales egresado del Instituto tecnológico de Mérida con el título de Maestro en Gestión de Tecnologías de la Información. Con amplia experiencia laboral que avalan los 17 años dedicados al desarrollo de software y más de 12 años en impartir cursos de programación y de tecnologías de la información,Actualmente se desempeña como programador Sr. en una empresa privada y docente de universidad, así como instructor en varias plataformas de enseñanza como son UDEMY, TEACHLRN, TUTELLUS'
+    },
+     {
+        id:12,
+        name:'Victor Robles',
+        job:'Laravel Developer',
+        image:'https://victorroblesweb.es/wp-content/uploads/2019/12/victor-robles-web-youtuber-desarrollador-web.png',
+        text:'En Víctor Robles Academy vas a encontrar los mejores cursos online de desarrollo web en Español. Más de 312.000 alumnos están aprendiendo programación web conmigo. Sinceramente, son el tipo de contenido que a mi me hubiera gustado encontrar cuando empecé en el mundo del desarrollo web profesional'
+    },
+     {
+        id:13,
+        name:'Nelson Jamal',
+        job:'Springboot developer',
+        image:'https://avatars.githubusercontent.com/u/5727929?v=4',
+        text:'As a Devops Engineer I mainly look after the infrastructure that powers all services across the organization. At Starling the culture is really nice. Everyone is nice to each other and more importantly we have fun. There is no blaming culture and we work together as a team to build software and fix issues when they happen'
+    },{
+        id:14,
+        name:'Kevin Powell',
+        job:'CSS developer',
+        image:'https://d33wubrfki0l68.cloudfront.net/3156dc4cf5f0f6b69cde5bdd18f0ff31eccb14e0/67c46/img/f21b00ed-300.jpeg',
+        text:'I learned to make websites on my own. I know how fun and rewarding it is, but I also know that it can be incredibly frustrating. I`m here to help make your journey into web development as easy and as fun '
+    },
+    {
+        id:15,
+        name:'Nicolas Schurman',
+        job:'AWS Developer',
+        image:'https://i.ytimg.com/vi/jaxjtywrG2A/hqdefault.jpg',
+        text:'He sido CTO y CEO de varias compañías de tecnología, relacionadas con cryptomonedas, geolocalización y comercio electrónico. ... Fui contratado en Nueva Zelanda cuando aún vivía en Chile, Actualmente soy desarrollador de software senior, instructor de tecnologías para el desarrollo web y móvil.'
+    }
+
+]
+
+
+export default reviews; 
